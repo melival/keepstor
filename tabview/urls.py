2a4ce60d3acfa3372623c4550adb2e24f791972e
melival <melival84@gmail.com>
2018-10-18 11:32:23 +0800

    release 0.1

--- tabview/urls.py
+++ tabview/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.show),
+    path('order_list', views.show_orderset),
 ]
--- tabview/views.py
+++ tabview/views.py
@@ -5,9 +5,18 @@ from . import keepstor
 # Create your views here.
 html_head = "<!DOCTYPE html><head><title>TableView</title></head>"
 html_open = "<html>"
+html_link = "<a href=\"order_list\">Спецификация</a> для заказа<br/>"
 html_close = "</html>"
+
 def show(request):
-    result = html_head + html_open
+    result = html_head + html_open + html_link
     result += keepstor.get_http_result_table()
+    result += html_close
+    return response(result)
+    
+def show_orderset(request):
+    result = html_head + html_open
+    with open("order_list.json") as f:
+        result = f.read()
     result += html_close
     return response(result)

d50a0f3fe11af6884dea57e7a914d014aa916eba
melival <36727073+melival@users.noreply.github.com>
2018-10-25 15:32:34 +0800

    Disable DEBUG

--- keepstor/settings.py
+++ keepstor/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '$f0hz()ojhc4dbaf%rbl4@cri6*u5$58674e2v^s64=rq$xk&#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'localhost', 
